5a1208693503493b1fe5d3381036d5cb2cadddec
“Samuel <iyanusamuel2004@gmail.com>
2025-08-31 10:17:19 +0100

    Added WelcomeMessage component and updated App.jsx with JSX changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,13 @@
+import WelcomeMessage from './components/WelcomeMessage';
+function App() {
+  return (
+    <>
+      <WelcomeMessage />
+    </>
+  );
+}
+
+export default App;
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
