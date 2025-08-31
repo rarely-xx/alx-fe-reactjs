7544e9aadc218b033abd24b360b85d0e06b59814
“Samuel <iyanusamuel2004@gmail.com>
2025-08-31 11:47:44 +0100

    Fix duplicate App component and add WelcomeMessage

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,23 +1,17 @@
-import WelcomeMessage from './components/WelcomeMessage';
-function App() {
-  return (
-    <>
-      <WelcomeMessage />
-    </>
-  );
-}
-
-export default App;
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <WelcomeMessage />
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -26,7 +20,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -35,6 +31,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
@@ -43,3 +40,4 @@ function App() {
 }
 
 export default App
+
