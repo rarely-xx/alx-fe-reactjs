706ddc34bd310ae8fe2fca461dd90c3f704ea0e1
“Samuel <iyanusamuel2004@gmail.com>
2025-09-06 11:16:13 +0100

    Added inline CSS styling to components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
+      <p>&copy; 2025 My Favorite Cities App</p>
+    </footer>
+  );
+}
+
+export default Footer;
