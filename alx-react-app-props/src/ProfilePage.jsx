9bb7a1e8cfeca86ae708b4d6af275070486a8f81
“Samuel <iyanusamuel2004@gmail.com>
2025-09-06 15:45:05 +0100

    Refactor app to use Context API instead of prop drilling

--- alx-react-app-props/src/ProfilePage.jsx
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -1,16 +1,7 @@
-import React, { useContext } from "react";
-import UserContext from "./UserContext";
+import UserInfo from "./UserInfo";
 
-const UserProfile = () => {
-  const user = useContext(UserContext);
+function ProfilePage() {
+  return <UserInfo />;
+}
 
-  return (
-    <div>
-      <h1>User Profile</h1>
-      <p><strong>Name:</strong> {user.name}</p>
-      <p><strong>Email:</strong> {user.email}</p>
-    </div>
-  );
-};
-
-export default UserProfile;
+export default ProfilePage;
