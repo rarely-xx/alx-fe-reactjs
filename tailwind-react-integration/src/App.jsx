fc2e667053a78b29a2876101778f228f35b0e8ec
“Samuel <iyanusamuel2004@gmail.com>
2025-09-24 00:01:23 +0100

    Add styled UserProfile component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,11 @@
+import UserProfile from "./components/UserProfile";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-600">
-      Tailwind is working! 🚀
-    </h1>
-  )
+    <div>
+      <UserProfile />
+    </div>
+  );
 }
 
-export default App
+export default App;
